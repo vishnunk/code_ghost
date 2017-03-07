3b3707e6bfb4cb44d7799d0a2cd614c7743ffb74
Vishnu N K <mails2vichu@gmail.com>
2017-03-07 14:06:11 +0530

    Added Question 2.

---
+++ 2.c
@@ -0,0 +1,14 @@
+/*
+ *  Program to reverse the words in a given line.(Without reversing the letters.)
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv){
+    char str[100];
+    printf("Enter the line to reverse words: ");
+    fgets(str, 100, stdin);
+        
+    return 0;
+}
