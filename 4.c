b9829458afd21df6e3d13690db3c7a18271974d3
Vishnu N K <vishnu.nk@cognizant.com>
2017-03-08 21:27:41 +0530

    Endianness check implemented.

--- 4.c
+++ 4.c
@@ -5,6 +5,15 @@
 #include <stdio.h>
 
 int main(int argc, char **argv){
+	int check = 0x12345678;
+
+	//Pointing address of check to char*.
+	char *ptr = (char *) &check;
+
+	if(*ptr == 0x12)
+		printf("The machine is Big-endian\n");
+	else 
+		printf("The machine is Little-endian\n");
 
 	return 0;
 }
