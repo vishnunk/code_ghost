18aaa1eab05105ffe9b7f948dd40ae33fa7200cd
Vishnu N K <mails2vichu@gmail.com>
2017-03-08 14:20:33 +0530

    Added Question 3

---
+++ 3.c
@@ -0,0 +1,12 @@
+/*
+ *  Program to set, clear, toggle the bits in a given integer.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv){
+    int x;
+    printf("Enter the integer value: ");
+    scanf("%d", &x);
+    return 0;
+}
