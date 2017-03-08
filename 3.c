20d4f9307c67556ed605412ddb018f2a658bfc4a
Vishnu N K <mails2vichu@gmail.com>
2017-03-08 15:55:00 +0530

    Question 3: Added structure to program

--- 3.c
+++ 3.c
@@ -5,8 +5,27 @@
 #include <stdio.h>
 
 int main(int argc, char **argv){
-    int x;
+    unsigned int x, operation, bit;
     printf("Enter the integer value: ");
     scanf("%d", &x);
+    printf("Enter the operation you want to perform:\n\t1. Set nth bit\n\t2. Clear nth bit\n\t3. Toggle nth bit\n");
+    scanf("%d", &operation);
+    printf("Enter the bit position where you want to perform the operation[0-7]: ");
+    scanf("%d", &bit);
+
+    if(operation == 1){
+        printf("Performing set operation on %dth bit\n", bit);
+        set_bit(&x, bit);
+    }
+    else if(operation == 2){
+        printf("Performing clear operation on %dth bit\n", bit);
+        clear_bit(&x, bit);
+    }
+    else if(operation == 3){
+        printf("Performing toggle operation on %dth bit\n", bit);
+        toggle_bit(&x, bit);
+    }
+    else
+        printf("Invalid operation. Please run the program again with proper input.\n");
     return 0;
 }
