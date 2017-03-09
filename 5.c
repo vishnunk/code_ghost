d37c5a5ab0605d67ee457276c945c5134e265358
Vishnu N K <mails2vichu@gmail.com>
2017-03-09 11:21:49 +0530

    sizeof operator implemented.

--- 5.c
+++ 5.c
@@ -4,7 +4,14 @@
 
 #include <stdio.h>
 
+#define do_sizeof(x) (char *)(&x + 1) - (char *)(&x)
+
 int main(int argc, char **argv){
-    
+    int integer;
+    double double_var;
+    char c;
+    printf("Size of int is    : %d\n", do_sizeof(integer));
+    printf("Size of double is : %d\n", do_sizeof(double_var));
+    printf("Size of char is    : %d\n", do_sizeof(c));
     return 0;
 }
