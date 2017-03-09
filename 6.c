779b11b61b625c8aa7a31087fe4bbfdc40b9a8a4
Vishnu N K <mails2vichu@gmail.com>
2017-03-09 12:15:54 +0530

    Added Question 6.

--- 6.c
+++ 6.c
@@ -5,6 +5,18 @@
 #include <stdio.h>
 
 int main(int argc, char **argv){
+    int size;
+    int i;
+    printf("Enter the size of the array: ");
+    scanf("%d", &size);
+    int array[size];
+    printf("Enter the array elements: \n");
+
+    for(i=0; i<size; i++)
+    {
+        printf("\t");
+        scanf("%d", &array[i]);
+    }
     
     return 0;
 }
